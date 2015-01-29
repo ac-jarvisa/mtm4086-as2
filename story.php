573c8f905e62060c8628eccc7c6f2047976ff416
Tyler Klepsch <klep0003@algonquinlive.com>
2015-01-29 18:58:00 -0500

    changed html to php file

---
+++ story.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Detective Stories</title>
+<!-- www.bootstrapcdn.com -->
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+<link href="styles/style.css" rel="stylesheet">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+        	<nav class="nav nav-justified">
+            <li class="active"><a href="#">Home</a></li>
+        	<li><a href="#">Navlink</a></li>
+        	<li><a href="#">Navlink</a></li>
+        	<li><a href="#">Navlink</a></li>
+            </nav>
+        
+        </header>
+    <div class="row">
+    	<div class="col-lg-12">Stuff</div>
+    </div>
+    
+    </div>
+</body>
+</html>
